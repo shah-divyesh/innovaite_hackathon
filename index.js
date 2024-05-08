bac4936ea86659059182f1976f982d55c0287d37
Pankhuri Gupta <34907504+pankhuri0209@users.noreply.github.com>
2024-05-08 00:11:55 -0400

    key removed

--- index.js
+++ index.js
@@ -4,16 +4,9 @@ const bodyParser = require("body-parser");
 
 const OpenAI = require("openai");
 
-// const config = new Configuration({
-//   apiKey: "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",
-//   //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
+// const openai = new OpenAI({
+//   apiKey: "your_key", // This is the default and can be omitted
 // });
-
-// const openai = new OpenAIApi(config);
-
-const openai = new OpenAI({
-  apiKey: "sk-proj-D0U2ULEB5I4f9rE2KbO3T3BlbkFJ9pH9JegvoEWffKHzvxpk", // This is the default and can be omitted
-});
 
 //setup server
 const app = express();
@@ -24,18 +17,7 @@ app.use(cors());
 app.post("/chat", async (req, res) => {
   const { prompt } = req.body;
 
-  //   const completion = await openai.createCompletion({
-  //     model: "text-davinci-003",
-  //     max_tokens: 512,
-  //     temperature: 0,
-  //     prompt: prompt,
-  //   });
-
   const completion = await openai.chat.completions.create({
-    // model: "text-davinci-003",
-    // max_tokens: 512,
-    // temperature: 0,
-    // prompt: prompt,
     messages: [{ role: "user", content: prompt }],
     model: "gpt-3.5-turbo",
   });
